7027ee58b7c4ac6779d6ed370a5f3f4e1c9c02ca
jrdesa <jrdesa@dpontespro.com>
2022-08-09 17:47:26 +0200

    Final

--- src/main/java/com/in/jrfc/exceptions/PrizeNotFoundException.java
+++ src/main/java/com/in/jrfc/exceptions/PrizeNotFoundException.java
@@ -4,8 +4,6 @@ import org.springframework.http.HttpStatus;
 
 public class PrizeNotFoundException extends RuntimeException {
     private static final String DESCRIPTION = "Prize Not Found Exception";
-
-
     public PrizeNotFoundException(HttpStatus notFound, String detail) {
 
         super(DESCRIPTION + ". " + detail);
